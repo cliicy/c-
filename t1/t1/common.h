420aeaec8085f84c9fa0338121cde968c57e5f3d
Julia <cliicy@gmail.com>
2018-04-02 12:10:40 +0800

    update something

--- t1/t1/common.h
+++ t1/t1/common.h
@@ -1,67 +1,160 @@
 #pragma once
-/*
-1字节     uint8_t
-2字节     uint16_t
-4字节     uint32_t
-8字节     uint64_t
-*/
 #include "..\t1\tree.h"
 #include "..\t1\EmployeeTree.h"
+#include "ListNode.h"
+#include "Node.h"
+#include "queue.h"
+#include "stack.h"
+
+
+using namespace linkedlistofclasses;
+using namespace linkedNodeofclasses;
+using namespace treetask;
+using namespace queuesavitch;
+using namespace stacksavitch;
 
 namespace solution{
-	//static int n; //定义静态全局变量
-//	extern int nA;
-//	void fn();
-	static void fnn();//声明静态函数
+	/*
+	1字节     uint8_t
+	2字节     uint16_t
+	4字节     uint32_t
+	8字节     uint64_t
+	*/
 
-	void fnn()//定义静态函数
+#define common_dir "..\\..\\common\\"
+#define infile "infile.txt"
+#define outfile "outfile.txt"
 
-	{
+#define MIN(A,B) ((A>=B?return A:return B))
 
-		int n = 10;
+	Node* addTwoNumbers(Node* n1, Node* n2);
+	ListNode* addxTwoNumbers(ListNode* l1, ListNode* l2);
+	ListNode* test1_addtwolink();
 
-		cout << n << endl;
+	ListNode* addEndTwoNumbers(ListNode* l1, ListNode* l2);
+	ListNode* test2_addtwolink();
 
-	}
+	bool isSameTree(TreeNode* p, TreeNode* q);
+	bool isScramble(string s1, string s2);
 
-	#define MIN(A,B) ((A>=B?return A:return B))
-	enum hasFile{ DOC=0, IMAGE };
+	bool isScramble1(string s1, string s2);
+	bool helper(string& a, int sa, int ea, string& b, int sb, int eb);
 
-	void Test1();
-	void GetMemory1(char* p);
-	char* GetMemory2();
-	void Test2();
-	void GetMemory3(char**p, int num);
-	void Test3();
-	void swap(int* p1, int* p2);
+	void transit_money();
+	int _strlen(const char *str);
 
-	void Func_size(char str[100]);
+	void fstropen();
+	void testcoutcin();
+	void multiplyBigInt();
+	void multiplyBigIntEx();
+	void head_insert(NodePtr& head, int the_number);
 
-	void LoopMove(char* pstr, int steps);
-	void LoopMove2(char* pstr, int steps);
-	void InitTree();
+	void demo_head_insert();
+	void do_stack();
+	void do_queue();
 
-	void InitEmployeeTree();
+	void do_vector();
+	void do_vector2();
 
-	void PrintTest(int nSpace);
+	void create_tree();
 
-	int recur_sum(int nNumber);
+void Test1();
+void GetMemory1(char* p);
+char* GetMemory2();
+void Test2();
+void GetMemory3(char**p, int num);
+void Test3();
+void swap(int* p1, int* p2);
 
-	typedef _Uint32t UIN16;
+void Func_size(char str[100]);
 
-	typedef struct tagWaveFormat {
-		char cRiff[4];
-		_Uint32t filelong;
-		char cWaveFlag[4];
-		char cFmtflag[4];
-		char cTransition[4];
-		UIN16 nFormatTag;
-		UIN16 nChannels;
-		UIN16 nSamplesPerSec;
-		UIN16 nAvgBytesperSec;
-		UIN16 nBlockAlign;
-		UIN16 nBitNumPerSample;
-		char cDataFlag[4];
-		UIN16 nAudioLength;
-	} WAVEFORMAT;
+void LoopMove(char* pstr, int steps);
+void LoopMove2(char* pstr, int steps);
+
+
+void check_crash_point();
+
+void check_crash_point2();
+
+void check_size();//测试指向字符数组的指针大小和指向字符的指针的大小
+static void fnn();//声明静态函数
+
+struct STest {
+	int i;
+	int * p;
+};
+
+
+struct PTest{
+	char c;
+	char *pc;
+};
+
+
+	
+
+#define MIN(A,B) ((A>=B?return A:return B))
+enum hasFile{ DOC=0, IMAGE };
+
+void Test1();
+void GetMemory1(char* p);
+char* GetMemory2();
+void Test2();
+void GetMemory3(char**p, int num);
+void Test3();
+void swap(int* p1, int* p2);
+
+void Func_size(char str[100]);
+
+void LoopMove(char* pstr, int steps);
+void LoopMove2(char* pstr, int steps);
+void InitTree();
+
+void InitEmployeeTree();
+
+void PrintTest(int nSpace);
+
+int recur_sum(int nNumber);
+
+typedef _Uint32t UIN16;
+
+typedef struct tagWaveFormat {
+	char cRiff[4];
+	_Uint32t filelong;
+	char cWaveFlag[4];
+	char cFmtflag[4];
+	char cTransition[4];
+	UIN16 nFormatTag;
+	UIN16 nChannels;
+	UIN16 nSamplesPerSec;
+	UIN16 nAvgBytesperSec;
+	UIN16 nBlockAlign;
+	UIN16 nBitNumPerSample;
+	char cDataFlag[4];
+	UIN16 nAudioLength;
+} WAVEFORMAT;
+
+
+//	int vac = 3;
+
+struct teststr{
+	int aa;
+	char a[3];
+	teststr(){
+		int i = 0;
+	};
+	~teststr(){
+
+	};
+};
+
+template <class T>
+int getArrayLen(T& array)
+{
+	return (sizeof(array) / sizeof(array[0]));
+}
+
+//	int removeDuplicates(vector<int>& nums);
+//	vector<int> twoSum(vector<int>& nums, int target);
+
 }
